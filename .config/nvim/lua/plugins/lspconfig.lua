0a11f57448902808d8a321edad7bd29721046157
nkarpuzov <ezio@chibiphoenix.com>
2024-11-20 12:02:20 +0200

    Lsp config changes

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -4,13 +4,25 @@ return {
     opts = {
       autoformat = false,
       servers = {
-        tsserver = {
+        ts_ls = {
           init_options = {
             preferences = {
               includeCompletionsForModuleExports = true,
               includeCompletionsForImportStatements = true,
               importModuleSpecifierPreference = "non-relative",
               -- other settings
+            },
+          },
+          settings = {
+            javascript = {
+              format = {
+                insertSpaceAfterKeywordsInControlFlowStatements = false, -- Disable space after keywords like 'if'
+              },
+            },
+            typescript = {
+              format = {
+                insertSpaceAfterKeywordsInControlFlowStatements = false, -- Disable space after keywords like 'if'
+              },
             },
           },
         },
