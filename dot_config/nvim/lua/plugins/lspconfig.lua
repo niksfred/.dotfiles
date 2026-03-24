a3c6b1e7297458605c69f8e4b2f3c4e23a56747a
Nikolay Karpuzov <nikolay.karpuzov@delasport.com>
2026-03-24 14:01:39 +0200

    Fixes node js version for vlts and adds zprofile.

--- dot_config/nvim/lua/plugins/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -2,7 +2,15 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
-      servers = {},
+      servers = {
+        vtsls = {
+          cmd = {
+            vim.fn.expand("~/.nvm/versions/node/v22.16.0/bin/node"),
+            vim.fn.expand("~/.local/share/nvim/mason/packages/vtsls/node_modules/@vtsls/language-server/dist/main.js"),
+            "--stdio",
+          },
+        },
+      },
     },
   },
 
